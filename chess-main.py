584c0ab9ae04372fbe6e1e2bf98c1af4c02924e8
ishilbhatia <124503060+ishilbhatia@users.noreply.github.com>
2024-04-09 23:07:51 +0530

    Add files via upload

---
+++ chess-main.py
@@ -0,0 +1,158 @@
+import pygame
+pygame.init()
+from vars import *
+ss=square_size
+screen=pygame.display.set_mode((ss*8,ss*8))
+import time
+from pieces import *
+
+BLACK=(0,0,0)
+WHITE = (255,255,255)
+
+r1=rook(0*ss,7*ss,screen,True,'rook')
+r2=rook(7*ss,7*ss,screen,True,'rook')
+r3=rook(0*ss,0*ss,screen,False,'rook')
+r4=rook(7*ss,0*ss,screen,False,'rook')
+q1=queen(3*ss,7*ss,screen,True,'queen')
+q2=queen(3*ss,0*ss,screen,False,'queen')
+k1=king(4*ss,0*ss,screen,False,'king')
+k2=king(4*ss,7*ss,screen,True,'king')
+n1=knight(1*ss,0*ss,screen,False,'knight')
+n2=knight(1*ss,7*ss,screen,True,'knight')
+n3=knight(6*ss,0*ss,screen,False,'knight')
+n4=knight(6*ss,7*ss,screen,True,'knight')
+b1=bishop(2*ss,7*ss,screen,True,'bishop')
+b2=bishop(2*ss,0*ss,screen,False,'bishop')
+b3=bishop(5*ss,7*ss,screen,True,'bishop')
+b4=bishop(5*ss,0*ss,screen,False,'bishop')
+p1=pawn(0*ss,6*ss,screen,True,'pawn')
+p2=pawn(1*ss,6*ss,screen,True,'pawn')
+p3=pawn(2*ss,6*ss,screen,True,'pawn')
+p4=pawn(3*ss,6*ss,screen,True,'pawn')
+p5=pawn(4*ss,6*ss,screen,True,'pawn')
+p6=pawn(5*ss,6*ss,screen,True,'pawn')
+p7=pawn(6*ss,6*ss,screen,True,'pawn')
+p8=pawn(7*ss,6*ss,screen,True,'pawn')
+pb1=pawn(0*ss,1*ss,screen,False,'pawn')
+pb2=pawn(1*ss,1*ss,screen,False,'pawn')
+pb3=pawn(2*ss,1*ss,screen,False,'pawn')
+pb4=pawn(3*ss,1*ss,screen,False,'pawn')
+pb5=pawn(4*ss,1*ss,screen,False,'pawn')
+pb6=pawn(5*ss,1*ss,screen,False,'pawn')
+pb7=pawn(6*ss,1*ss,screen,False,'pawn')
+pb8=pawn(7*ss,1*ss,screen,False,'pawn')
+dummy=pawn(10*ss,1*ss,screen,False,'pawn')
+
+
+
+pieces = [dummy,r1,r2,r3,r4,q1,q2,k1,k2,n1,n2,n3,n4,b1,b2,b3,b4,p1,p2,p3,p4,p5,p6,p7,p8,pb1,pb2,pb3,pb4,pb5,pb6,pb7,pb8]
+pawns=[p1,p2,p3,p4,p5,p6,p7,p8,pb1,pb2,pb3,pb4,pb5,pb6,pb7,pb8]
+white=[]
+black=[]
+for piece in pieces:
+    if piece.white:
+        white.append(piece)
+    else:
+        black.append(piece)
+#print (white)
+#print(black)
+def drawBoard():
+    screen.fill(WHITE)
+    for i in range(8):
+        if(i%2==1):
+            for j in range(4):
+                pygame.draw.rect(screen, BLACK, (j*2*ss,i*ss,ss, ss))
+        else:
+            for j in range(1,5):
+                pygame.draw.rect(screen, BLACK, (j*2*ss - ss,i*ss,ss, ss))
+
+running=True
+move=0
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running=False
+    drawBoard()
+    for pawny in pawns:
+        if pawny.__getattribute__('dragy')==7*ss and pawny.__getattribute__('type')=='pawn':
+            pawny=queen(pawny.__getattribute__('dragx'),0,screen,False,'queen')
+    
+        if pawny.__getattribute__('dragy')==0 and pawny.__getattribute__('type')=='pawn':
+            b=0
+            c=0
+            for i in range(len(pieces)):
+                if pieces[i]==pawny:
+                    c=i
+                    pieces[i]=None
+                    #pieces[i]=pawny
+                    break
+            for i in range(len(pawns)):
+                if pawns[i]==pawny:
+                    b=i
+                    #pieces[i]=pawny
+                    break
+            pawny.dragy=-1*ss
+            pawny.draw()
+            
+            pawny=queen(pawny.__getattribute__('dragx'),0,screen,True,'queen')
+            
+            #pieces[c]=pawny
+            pieces.append(pawny)
+            pawns[b]=pawny
+            
+            pawny.highlight()
+            pawny.click(pieces)
+            pawny.draw()
+
+            #pieces.append(pawny)
+    '''if move%2==0:
+        for i in range(len(white)):
+            if None != white[i]:
+                white[i].highlight()
+                white[i].click(white)
+                white[i].draw()
+                if white[i].turn:
+                    
+                    for x in range(len(pieces)):
+                        if (i != x) and pieces[x] != None:
+                            if (pieces[i].rect.y == pieces[x].rect.y) and (pieces[i].rect.x == pieces[x].rect.x):
+                                pieces[x] = None
+                                white[i].turn = False
+                    move=move+1
+    else:
+        for i in range(len(black)):
+            if None != black[i]:
+                black[i].highlight()
+                black[i].click(black)
+                black[i].draw()
+                if black[i].turn:
+                    #move=move+1
+                    for x in range(len(pieces)):
+                        if (i != x) and pieces[x] != None:
+                            if (pieces[i].rect.y == pieces[x].rect.y) and (pieces[i].rect.x == pieces[x].rect.x):
+                                pieces[x] = None
+                                black[i].turn = False
+                    move=move+1
+    '''
+    for i in range(len(pieces)):
+            if None != pieces[i]:
+                pieces[i].highlight()
+                pieces[i].click(pieces)
+                pieces[i].draw()
+                if pieces[i].turn:
+                    for x in range(len(pieces)):
+                        if (i != x) and pieces[x] != None:
+                            if (pieces[i].rect.y == pieces[x].rect.y) and (pieces[i].rect.x == pieces[x].rect.x):
+                                pieces[x] = None
+                                pieces[i].turn = False
+    for i in pieces:
+        if i != None:
+            i.turn = False
+    for piece in pieces:
+        if piece !=None:
+            piece.draw()
+    
+    #print (move)
+
+    pygame.display.update()
+pygame.quit()
---
+++ pieces.py
@@ -0,0 +1,281 @@
+import pygame
+pygame.init()
+
+from utils import *
+from utils import *
+
+class base():
+    def __init__(self, x, y, surface, white,type):
+        self.win = surface
+        if white:
+            self.white = True
+        else:
+            self.white = False
+        self.rect = pygame.Rect(x, y, square_size, square_size)
+        self.dragx, self.dragy  = x, y
+        self.selected = False
+        self.move = False
+        self.turn = False
+        self.image()
+        self.type=type
+    def image(self):
+        pass
+    def restrict(self, x, y, pieces_list):
+        pass
+    def rules(self,x,y, pieces_list):
+        pass   
+    def highlight(self):
+        if self.selected:
+            pygame.draw.rect(self.win, (0, 255, 0), (self.dragx, self.dragy, square_size, square_size))
+
+    def draw(self):
+        self.win.blit(self.img, self.rect)
+
+    def click(self, pieces_list):
+        if pygame.mouse.get_pressed()[0] and pygame.Rect.collidepoint(self.rect, pygame.mouse.get_pos()) and not self.selected and not self.move:
+            self.selected = True
+            self.dragx, self.dragy = self.rect.x, self.rect.y
+
+        if self.selected and not self.move:
+            if pygame.mouse.get_pressed()[0] and not (pygame.Rect.collidepoint(self.rect, pygame.mouse.get_pos())):
+                mousex, mousey = get_coords(pygame.mouse.get_pos()[0], pygame.mouse.get_pos()[1])
+                if self.rules(mousex, mousey, pieces_list) and self.restrict(mousex, mousey, pieces_list) and self.check(pieces_list):
+                    self.rect.x, self.rect.y = mousex, mousey
+                    self.move = True
+                    self.turn = True
+
+        if self.move and self.selected:
+            if not pygame.mouse.get_pressed()[0]:# and not pygame.Rect.collidepoint(self.rect, pygame.mouse.get_pos()):
+                self.move = False
+                self.selected = False
+
+        if pygame.mouse.get_pressed()[0]:
+            if self.selected and not self.move and not (pygame.Rect.collidepoint(self.rect, pygame.mouse.get_pos())):
+                self.selected = False
+        
+    def check(self,pieces_list):
+        
+        for piece in pieces_list:
+            if piece!=None:
+                if piece.white==self.white and piece.type=='king':
+                    k=king(piece.rect.x,piece.rect.y,piece.win,self.white,'king')
+        for piece in pieces_list:
+            if piece!=None:
+                if piece.rules(k.rect.x,k.rect.y,pieces_list) and piece.restrict(k.rect.x,k.rect.y,pieces_list):
+                    pygame.draw.rect(self.win, (255, 0, 0), (self.dragx, self.dragy, square_size, square_size))
+                    print("check")
+                    return False
+        return True
+
+
+
+class rook(base):
+    def image(self):
+        if self.white:
+            self.img = pygame.transform.scale(pygame.image.load("images/w-rook.png"), (square_size, square_size))
+        else:
+            self.img = pygame.transform.scale(pygame.image.load("images/b-rook.png"), (square_size, square_size))
+            self.white = False
+
+    def restrict(self, x, y, pieces_list):
+        for piece in pieces_list:
+            if piece != None:
+                if x == piece.rect.x:
+                    if self.rect.y > piece.rect.y:
+                        if y<piece.rect.y:
+                            return False
+                    elif self.rect.y < piece.rect.y:
+                        if y>piece.rect.y:
+                            return False
+                elif y == piece.rect.y:
+                    if self.rect.x > piece.rect.x:
+                        if x<piece.rect.x:
+                            return False
+                    elif self.rect.x < piece.rect.x:
+                        if x>piece.rect.x:
+                            return False
+        return True
+
+    def rules(self,x,y, pieces_list):
+        if (((x==self.rect.x) or (y==self.rect.y)) and not((x==self.rect.x) and (y==self.rect.y))):
+            for i in pieces_list:
+                if i != None:
+                    if (i.rect.x == x) and (i.rect.y == y):
+                        if i.white == self.white:
+                            return False 
+            return True
+        return False
+
+class bishop(base):
+    def image(self):
+        if self.white:
+            self.img = pygame.transform.scale(pygame.image.load("images/w-bishop.png"), (square_size, square_size))
+        else:
+            self.img = pygame.transform.scale(pygame.image.load("images/b-bishop.png"), (square_size, square_size))
+            self.white = False
+
+    def restrict(self, x, y, pieces_list):
+        ydiff = 1 if self.rect.y < y else -1
+        xdiff = 1 if self.rect.x < x else -1
+        for i in range(square_size, (abs(x - self.rect.x)), square_size):
+            for piece in pieces_list:
+                if piece != None:
+                    if (piece.rect.y == (self.rect.y + (i*ydiff))) and (piece.rect.x == (self.rect.x + (i*xdiff)) and not(piece.rect.y==y and piece.rect.x==x)):
+                        return False
+        return True
+
+
+    def rules(self,x,y, pieces_list):
+        if abs(self.rect.x - x) == abs(self.rect.y - y):
+            for i in pieces_list:
+                if i != None:
+                    if (i.rect.x == x) and (i.rect.y == y):
+                        if i.white == self.white:
+                            return False 
+            return True
+        return False
+    
+class knight(base):
+    def image(self):
+        if self.white:
+            self.img = pygame.transform.scale(pygame.image.load("images/w-knight.png"), (square_size, square_size))
+        else:
+            self.img = pygame.transform.scale(pygame.image.load("images/b-knight.png"), (square_size, square_size))
+            self.white = False
+
+    def restrict(self, x, y, pieces_list):
+        return True
+    
+    def rules(self, x, y, pieces_list):
+        if ((abs(self.rect.x-x)<=2*square_size and abs(self.rect.y-y)<=2*square_size and (abs(self.rect.x-x)+abs(self.rect.y-y))==3*square_size) and not((x==self.rect.x) and (y==self.rect.y))):
+            for i in pieces_list:
+                if i != None:
+                    if (i.rect.x == x) and (i.rect.y == y):
+                        if i.white == self.white:
+                            return False 
+            return True
+        return False
+    
+class queen(base):
+    def image(self):
+        if self.white:
+            self.img = pygame.transform.scale(pygame.image.load("images/w-queen.png"), (square_size, square_size))
+        else:
+            self.img = pygame.transform.scale(pygame.image.load("images/b-queen.png"), (square_size, square_size))
+            self.white = False
+
+    def restrict(self, x, y, pieces_list):
+        rookrestrict, bishoprestrict = rook(self.rect.x, self.rect.y, self.win, self.white,'rook'), bishop(self.rect.x, self.rect.y, self.win, self.white,'bishop')
+        return rookrestrict.restrict(x, y, pieces_list) and bishoprestrict.restrict(x, y, pieces_list)
+
+    def rules(self,x,y, pieces_list):
+        rookRules, bishoprules = rook(self.rect.x, self.rect.y, self.win, self.white,'rook'), bishop(self.rect.x, self.rect.y, self.win, self.white,'bishop')
+        if rookRules.rules(x, y, pieces_list) or bishoprules.rules(x, y, pieces_list):
+            for i in pieces_list:
+                if i != None:
+                    if (i.rect.x == x) and (i.rect.y == y):
+                        if i.white == self.white:
+                            return False 
+            return True
+        return False
+
+class king(base):
+    def image(self):
+        if self.white:
+            self.img = pygame.transform.scale(pygame.image.load("images/w-king.png"), (square_size, square_size))
+        else:
+            self.img = pygame.transform.scale(pygame.image.load("images/b-king.png"), (square_size, square_size))
+            self.white = False
+
+    def restrict(self, x, y, pieces_list):
+        for index, piece in enumerate(pieces_list):
+            try:
+                piece_name = (str(piece).strip('<').split()[0].split('.')[1])
+            except IndexError:
+                piece_name = None
+            if piece != None and piece_name != None:
+                if piece.white != self.white:
+                    if piece_name == 'king':
+                        if piece.rules(x,y,pieces_list):
+                            return False
+                    if piece.rules(x,y,pieces_list) and piece.restrict(x,y,pieces_list):
+                        print("False for ", piece)
+                        return False
+            if x == piece.rect.x and y == piece.rect.y:
+                withoutPiece = pieces_list[:index] + pieces_list[index+1:]
+                for other_piece in withoutPiece:
+                    if other_piece is not None and other_piece.white != self.white:
+                        if other_piece.rules(x, y, withoutPiece) and other_piece.restrict(x, y, withoutPiece):
+                            print("False for", other_piece)
+                            return False
+        return True
+    
+    def rules(self, x, y, pieces_list):
+        if abs(self.rect.x - x) <= square_size and abs(self.rect.y - y) <= square_size:
+            for i in pieces_list:
+                if i != None:
+                    if (i.rect.x == x) and (i.rect.y == y):
+                        if i.white == self.white:
+                            return False 
+            return True
+        return False
+class pawn(base):
+    def image(self):
+        if self.white:
+            self.img = pygame.transform.scale(pygame.image.load("images/w-pawn.png"), (square_size, square_size))
+        else:
+            self.img = pygame.transform.scale(pygame.image.load("images/b-pawn.png"), (square_size, square_size))
+            self.white = False
+
+    def checkColor(self, x, y, pieces_list):
+        return True
+    def restrict(self, x, y, pieces_list):
+        return True
+    def rules(self,x,y, pieces_list):
+        #print(self.dragx,self.dragy,x,y)
+        if self.white:
+            if self.dragy==1*square_size or self.dragy==6*square_size:
+                if ((self.dragy-y<=2*square_size and self.dragx==x) and not((x==self.dragx) and (y==self.dragy))):
+                    if self.checkColor(x, y, pieces_list):
+                        for i in pieces_list:
+                            if i != None:
+                                if (i.rect.x == x) and (i.rect.y == y):
+                                    if i.white == self.white:
+                                        return False 
+                    return True
+                else:
+                    return False
+            else:
+                if ((self.dragy-y==1*square_size and self.dragx==x) and not((x==self.dragx) and (y==self.dragy))):
+                    #add anathor if for checking valid moves(respective to other pieces on board)
+                    if self.checkColor(x, y, pieces_list):
+                        for i in pieces_list:
+                            if i != None:
+                                if (i.rect.x == x) and (i.rect.y == y):
+                                    if i.white == self.white:
+                                        return False 
+                    return True
+                return False
+        else:
+            if self.dragy==1*square_size or self.dragy==6*square_size:
+                if ((self.dragy-y>=-2*square_size and self.dragx==x) and not((x==self.dragx) and (y==self.dragy))):
+                    if self.checkColor(x, y, pieces_list):
+                        for i in pieces_list:
+                            if i != None:
+                                if (i.rect.x == x) and (i.rect.y == y):
+                                    if i.white == self.white:
+                                        return False 
+                    return True
+                else:
+                    return False
+            else:
+                if ((self.dragy-y==-1*square_size and self.dragx==x) and not((x==self.dragx) and (y==self.dragy))):
+                    #add anathor if for checking valid moves(respective to other pieces on board)
+                    if self.checkColor(x, y, pieces_list):
+                        for i in pieces_list:
+                            if i != None:
+                                if (i.rect.x == x) and (i.rect.y == y):
+                                    if i.white == self.white:
+                                        return False 
+                    return True
+                return False
